d0d019d7eab0c92e2bb9e40c23a86a502a469405
Nikolajus <nikolajus@gmail.com>
2012-07-20 14:32:22 +0200

    template fixes to redirect stuff

--- weddings/views.py
+++ weddings/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 
 class PinView(TemplateView):
     template_name = "pin1.html"
+
+    def post(self, request):
+        return HttpResponseRedirect(reverse('pin2'))
+
+    def get_context_data(self, **kwargs):
+        context = super(PinView, self).get_context_data(**kwargs)
+        return context
