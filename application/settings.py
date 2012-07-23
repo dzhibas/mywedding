4168de6525903d6c9e02dba840f2f19cdb146665
Nikolajus <nikolajus@gmail.com>
2012-07-23 10:14:12 +0200

    Enable flat pages to store information about wedding

--- application/settings.py
+++ application/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.markup',
     'django.contrib.admin',
+    'django.contrib.flatpages',
     'south',
     'django_markdown',
     'debug_toolbar',
